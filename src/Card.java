f1b104c8ecb9beff04ab5651fb6466b3339f369d
r.zhao67 <r.zhao67@berkeley.edu>
2019-08-25 18:56:56 -0700

    Add basic implementation

---
+++ src/Card.java
@@ -0,0 +1,23 @@
+public class Card {
+    private int color;
+    private int fill;
+    private int shape;
+    private int count;
+
+    Card(int c, int f, int s, int num) {
+        color = c;
+        fill = f;
+        shape = s;
+        count = num;
+    }
+
+    public boolean equals(Card a, Card b) {
+        return a.color == b.color && a.fill == b.fill &&
+                a.shape == b.shape && a.count == b.count;
+    }
+
+    public int hashCode(Card c) {
+        return (int) ((c.color * Math.pow(3, 4)) + (c.fill * Math.pow(3, 3)) +
+                (c.shape * Math.pow(3, 2)) + (c.count * 3));
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Random;
+
+public class Game {
+    private Set<Card> board = new HashSet<>();
+
+    public void addCard(Set board) {
+        Random randNums = new Random(1);
+        int randColor = randNums.nextInt(4);
+        int randFill = randNums.nextInt(4);
+        int randShape = randNums.nextInt(4);
+        int randCount = randNums.nextInt(4);
+        Card newCard = new Card(randColor, randFill, randShape, randCount);
+        board.add(newCard);
+    }
+
+    public void initializeBoard() {
+        while (board.size() < 12) {
+            addCard(board);
+        }
+    }
+}
